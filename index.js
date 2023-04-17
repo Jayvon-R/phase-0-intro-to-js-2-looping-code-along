701cde0274a25bd4736ecae81297ff2faf3b1125
Jayvon Rhines <jayvonrhines0@gmail.com>
2023-04-16 20:19:41 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names){
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+
+function countDown(num){
+  while (num >= 0){
+    console.log(num)
+    num--;
+  }
+}
